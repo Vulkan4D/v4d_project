25d8008d40a31f684093f07f3ad85a07ced93bc1
olivier st-laurent <kzaurckichz@gmail.com>
2019-08-15 16:49:08 -0400

    updated standards

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "v4d.h"
 
-int main(int argc, char **args) {
-    std::cout << "Hello V4D !" << std::endl;
+int main(int argc, char** args) {
+	std::cout << "Hello V4D !" << std::endl;
 
-    std::cin.get();
+	std::cin.get();
 }
