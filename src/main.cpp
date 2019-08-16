64fc8519c6015240ebaef3a939890bb9c32d5d1e
olivier st-laurent <kzaurckichz@gmail.com>
2019-08-16 15:30:35 -0400

    WIP events

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include "v4d.h"
 
-// v4d::event::V4D_CORE_INIT << []{LOG("Initialized V4D")};
-
 int main(int argc, char** args) {
+	v4d::event::V4D_CORE_INIT << [](void*){LOG("Initialized V4D")};
+
 	v4d::Init();
+	
 	std::cout << "Hello V4D !" << std::endl;
 
 	std::cin.get();
