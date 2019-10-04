102298377034209387abeaa57aa52111f79ebf0a
olivier st-laurent <kzaurckichz@gmail.com>
2019-10-04 15:55:55 -0400

    fixed potential future issues

--- src/config.hh
+++ src/config.hh
@@ -1,9 +1,9 @@
 #pragma once
 
 // #define ZAP_USE_REINTERPRET_CAST_INSTEAD_OF_MEMCPY
+// #define EVENT_DEFINITIONS_VARIADIC
 
 // Logger
 #define LOGGER_INSTANCE v4d::io::Logger::ConsoleInstance()
 
-// #define EVENT_DEFINITIONS_VARIADIC
 
