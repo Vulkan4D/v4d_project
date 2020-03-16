8a478c8d5300868e391caf8186d53bc5b7db750a
olivier st-laurent <kzaurckichz@gmail.com>
2020-03-16 03:49:36 -0400

    WIP ray tracing renderer

--- src/v4d/modules/test_planets_rtx/module.cpp
+++ src/v4d/modules/test_planets_rtx/module.cpp
@@ -15,48 +15,72 @@ using namespace v4d::graphics::vulkan::rtx;
 class Rendering : public v4d::modules::Rendering {
 public:
 	Rendering() {}
-
 	int OrderIndex() const override {return 0;}
+
+	// Init/Shaders
+	void Init() override {
+		
+	}
+	void InitLayouts(std::vector<DescriptorSet*>&, std::unordered_map<std::string, Image*>&) override {
+		
+	}
+	void ConfigureShaders(std::unordered_map<std::string, std::vector<RasterShaderPipeline*>>& shaders, ShaderBindingTable* shaderBindingTable) override {
+		Geometry::rayTracingShaderOffsets["planet"] = shaderBindingTable->AddHitShader("modules/test_planets_rtx/assets/shaders/planets.rchit", "", "modules/test_planets_rtx/assets/shaders/planets.rint");
+		
+	}
+	void ReadShaders() override {
+		
+	}
 	
+	// Images / Buffers
+	void CreateResources() override {
+		
+	}
+	void DestroyResources() override {
+		
+	}
+	void AllocateBuffers() override {
+		
+	}
+	void FreeBuffers() override {
+		
+	}
+	
+	// Scene
+	void LoadScene(Scene& scene) {
+		
+		// Light source
+		scene.objectInstances.emplace_back(new ObjectInstance("light"))->Configure([](ObjectInstance* obj){
+			obj->SetSphereLightSource(20, 100000000);
+		}, {10,-1000,300});
+		
+		// Planet
+		scene.objectInstances.emplace_back(new ObjectInstance("planet"))->Configure([](ObjectInstance* obj){
+			obj->SetSphereGeometry(200, {1,0,0, 1});
+		}, {0,400,0});
+		
+	}
+
+	// Frame Update
+	void FrameUpdate(Scene&) override {
+		
+	}
+	void LowPriorityFrameUpdate() override {
+		
+	}
+	
+	void RunDynamicLowPriorityCompute(VkCommandBuffer) override {
+		
+	}
+	void RunDynamicLowPriorityGraphics(VkCommandBuffer) override {
+		
+	}
+
 	#ifdef _ENABLE_IMGUI
 		void RunImGui() override {
 			
 		}
 	#endif
-	
-	virtual void ConfigureShaders(std::unordered_map<std::string, std::vector<RasterShaderPipeline*>>& shaders, ShaderBindingTable* shaderBindingTable) override {
-		// Geometry::rayTracingShaderOffsets["planet"] = shaderBindingTable->AddHitShader("planet.rchit", "", "planet.rint");
-		
-	}
-	
-	void LoadScene(Scene& scene) {
-		
-		// scene.objectInstances.emplace_back(new ObjectInstance())->Configure(CreateTestBox, {0,250,-30}, 180.0);
-		// scene.objectInstances.emplace_back(new ObjectInstance())->Configure(CreateTestBox, {200,250,-30}, 120.0);
-		// scene.objectInstances.emplace_back(new ObjectInstance())->Configure(CreateTestBox, {-200,250,-30}, -120.0);
-		
-		// scene.objectInstances.emplace_back(new ObjectInstance("light"))->Configure([](ObjectInstance* obj){
-		// 	obj->SetSphereLightSource(20, 10000000);
-		// }, {10,-2000,10});
-		// scene.objectInstances.emplace_back(new ObjectInstance("light"))->Configure([](ObjectInstance* obj){
-		// 	obj->SetSphereLightSource(200, 100000000);
-		// }, {10,-500,1000});
-		// scene.objectInstances.emplace_back(new ObjectInstance("light"))->Configure([](ObjectInstance* obj){
-		// 	obj->SetSphereLightSource(2, 10000);
-		// }, {10,270,10});
-		// scene.objectInstances.emplace_back(new ObjectInstance("light"))->Configure([](ObjectInstance* obj){
-		// 	obj->SetSphereLightSource(1, 10000);
-		// }, {210,270,-20});
-		// scene.objectInstances.emplace_back(new ObjectInstance("light"))->Configure([](ObjectInstance* obj){
-		// 	obj->SetSphereLightSource(1, 10000);
-		// }, {-190,270,-15});
-		
-		// scene.objectInstances.emplace_back(new ObjectInstance("sphere"))->Configure([](ObjectInstance* obj){
-		// 	obj->SetSphereGeometry(50, {1,0,0, 1});
-		// }, {60,300,500});
-		
-	}
-	
 };
 
 V4DMODULE void V4D_ModuleCreate() {
