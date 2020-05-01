1789bbea0b194e6e53c19f504561f996e29310c1
olivier st-laurent <kzaurckichz@gmail.com>
2020-05-01 00:01:43 -0400

    improved shader compilation and watch script

--- crosscompile/windows/copy_shaders.sh
+++ crosscompile/windows/copy_shaders.sh
@@ -1,16 +1,9 @@
 cd "`dirname $0`/../.."
 
-# while inotifywait -e close_write '/home/olivier/projects/v4d_project/build/debug/modules/test_planets_rtx/assets/shaders/planets.meta'; do 
-# 	scp -rq build/debug/modules/test_planets_rtx/assets/shaders/*.spv WINDOWS_PC:/v4d_build/debug/modules/test_planets_rtx/assets/shaders/
-# 	scp -rq build/debug/modules/test_planets_rtx/assets/shaders/*.meta WINDOWS_PC:/v4d_build/debug/modules/test_planets_rtx/assets/shaders/
-# done
-
-# while inotifywait -e close_write '/home/olivier/projects/v4d_project/build/debug/modules/test_planets_rtx/assets/shaders/planetAtmosphere.meta'; do 
-# 	scp -rq build/debug/modules/test_planets_rtx/assets/shaders/*.spv WINDOWS_PC:/v4d_build/debug/modules/test_planets_rtx/assets/shaders/
-# 	scp -rq build/debug/modules/test_planets_rtx/assets/shaders/*.meta WINDOWS_PC:/v4d_build/debug/modules/test_planets_rtx/assets/shaders/
-# done
-
-# while inotifywait -e close_write '/home/olivier/projects/v4d_project/build/debug/incubator_rendering/assets/shaders/v4d_postProcessing.meta'; do 
-# 	scp -rq build/debug/incubator_rendering/assets/shaders/*.spv WINDOWS_PC:/v4d_build/debug/incubator_rendering/assets/shaders/
-# 	scp -rq build/debug/incubator_rendering/assets/shaders/*.meta WINDOWS_PC:/v4d_build/debug/incubator_rendering/assets/shaders/
-# done
+inotifywait -e close_write "$1.meta"
+if [[ -e "$1.meta" ]] ; then
+	dir="`dirname $1`/"
+	scp -rq "$1".*.spv WINDOWS_PC:/v4d_$dir
+	scp -rq "$1.meta" WINDOWS_PC:/v4d_$dir
+	sh -c "$0 \"$1\"" &
+fi
--- watchShaders.sh
+++ watchShaders.sh
@@ -1,3 +1,21 @@
 cd `dirname $0`
-find build/debug -type f -name "*.watch.sh" -exec sh -c '{} &' \;
-sh -c crosscompile/windows/copy_shaders.sh &
+
+#rebuild shaders
+cd build && rm -f CMakeCache.txt ; 
+find . -type d -name 'shaders' | xargs rm -rf ; 
+killall inotifywait
+sleep 1
+cmake .. -DCMAKE_BUILD_TYPE=Debug ; 
+make shaders -j8;
+
+cd ..
+
+for dir in `find build/debug -type d -name 'shaders'` ; do
+	for f in `find "$dir" -type f -name '*.watch.sh'` ; do
+		sh -c "$f" &
+	done
+	for f in `find "$dir" -type f -name '*.meta'` ; do
+		shader=`echo "$f" | sed 's/\.meta$//'`
+		sh -c "crosscompile/windows/copy_shaders.sh \"$shader\"" &
+	done
+done
