322c76768794b790405853d3ae18931ee2361b04
olivier st-laurent <kzaurckichz@gmail.com>
2020-05-22 00:24:12 -0400

    WIP refactoring renderer

--- src/v4d/modules/V4D_hybrid/assets/shaders/raster_lighting.glsl
+++ src/v4d/modules/V4D_hybrid/assets/shaders/raster_lighting.glsl
@@ -1,5 +1,42 @@
 #include "core.glsl"
 
+#common rtx.*
+#include "rtx_base.glsl"
+
+#common .*frag
+#include "v4d/modules/V4D_hybrid/glsl_includes/pl_lighting_raster.glsl"
+#include "v4d/modules/V4D_hybrid/glsl_includes/raster_pbr.glsl"
+void StandardPbrFragmentLightingShader() {
+	float depth = GetDepth();
+	if (depth == 0) {
+		// There is nothing here
+		WriteLitImage(vec4(0));
+		discard;
+	}
+	
+	ReadPbrGBuffers();
+	
+	if (DebugWireframe) {
+		WriteLitImage(vec4(pbrGBuffers.viewSpaceNormal, 1));
+		return;
+	}
+	
+	vec3 litColor = vec3(0);
+	
+	// Emitter
+	if (pbrGBuffers.emit > 0) {
+		litColor += pbrGBuffers.albedo * pbrGBuffers.emit;
+	}
+	
+	// PBR
+	if (dot(pbrGBuffers.viewSpaceNormal, pbrGBuffers.viewSpaceNormal) > 0) {
+		litColor += ApplyPBRShading(pbrGBuffers.viewSpacePosition, pbrGBuffers.albedo, pbrGBuffers.viewSpaceNormal, /*bump*/vec3(0), pbrGBuffers.roughness, pbrGBuffers.metallic);
+	}
+	
+	WriteLitImage(vec4(litColor, 1));
+}
+
+
 #################################################################################
 #shader vert
 
@@ -11,74 +48,14 @@ void main() {
 }
 
 #################################################################################
+
 #shader frag
-
-#include "v4d/modules/V4D_hybrid/glsl_includes/pl_lighting_raster.glsl"
-#include "v4d/modules/V4D_hybrid/glsl_includes/raster_pbr.glsl"
-
 void main() {
-	float depth = GetDepth();
-	if (depth == 0) {
-		// There is nothing here
-		WriteLitImage(vec4(0));
-		discard;
-	}
-	
-	ReadPbrGBuffers();
-	
-	if (DebugWireframe) {
-		WriteLitImage(vec4(pbrGBuffers.viewSpaceNormal, 1));
-		return;
-	}
-	
-	vec3 litColor = vec3(0);
-	
-	// Emitter
-	if (pbrGBuffers.emit > 0) {
-		litColor += pbrGBuffers.albedo * pbrGBuffers.emit;
-	}
-	
-	// PBR
-	if (dot(pbrGBuffers.viewSpaceNormal, pbrGBuffers.viewSpaceNormal) > 0) {
-		litColor += ApplyPBRShading(pbrGBuffers.viewSpacePosition, pbrGBuffers.albedo, pbrGBuffers.viewSpaceNormal, /*bump*/vec3(0), pbrGBuffers.roughness, pbrGBuffers.metallic);
-	}
-	
-	WriteLitImage(vec4(litColor, 1));
+	StandardPbrFragmentLightingShader();
 }
 
-#################################################################################
 #shader rtx.frag
-
-#include "rtx_base.glsl"
-#include "v4d/modules/V4D_hybrid/glsl_includes/pl_lighting_raster.glsl"
-#include "v4d/modules/V4D_hybrid/glsl_includes/raster_pbr.glsl"
-
 void main() {
-	float depth = GetDepth();
-	if (depth == 0) {
-		// There is nothing here
-		WriteLitImage(vec4(0));
-		discard;
-	}
-	
-	ReadPbrGBuffers();
-	
-	if (DebugWireframe) {
-		WriteLitImage(vec4(pbrGBuffers.viewSpaceNormal, 1));
-		return;
-	}
-	
-	vec3 litColor = vec3(0);
-	
-	// Emitter
-	if (pbrGBuffers.emit > 0) {
-		litColor += pbrGBuffers.albedo * pbrGBuffers.emit;
-	}
-	
-	// PBR
-	if (dot(pbrGBuffers.viewSpaceNormal, pbrGBuffers.viewSpaceNormal) > 0) {
-		litColor += ApplyPBRShading(pbrGBuffers.viewSpacePosition, pbrGBuffers.albedo, pbrGBuffers.viewSpaceNormal, /*bump*/vec3(0), pbrGBuffers.roughness, pbrGBuffers.metallic);
-	}
-	
-	WriteLitImage(vec4(litColor, 1));
+	StandardPbrFragmentLightingShader();
 }
+
